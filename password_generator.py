2203c94be8ba07c667471112443f070ad365d994
Sneha <snehart07@gmail.com>
2025-10-30 02:01:08 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,12 +2,12 @@ import random
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 symbols = ['*','@','$','^','&','#','!','~','_']
-numbers = ['1','2','3','4','5','6','7','8','9']
+numbers = ['1','2','3','4','5','6','7','8','9','0']
 print("Welcome to password generator")
 
 l = int(input("Enter number of letters in password\n"))
 s = int(input("Enter number of symbols in password\n"))
-n = int(input("Enter total numbers in password\n"))
+n = int(input("Enter count of numbers in password\n"))
 
 password = []
 for char in range(1,l+1):
@@ -21,4 +21,5 @@ p = ""
 for char in password:
     p += char
 print(f"your password is {p}\n")
+
 print("THANK YOU!!!")
